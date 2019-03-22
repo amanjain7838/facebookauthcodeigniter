b9260b12285739c63aa4d80b4661f677c79cb2d8
Aman Jain <amanjain7838@gmail.com>
2019-03-22 23:04:18 +0530

    second commit:ignoring config

--- application/controllers/Loginflow.php
+++ application/controllers/Loginflow.php
@@ -20,15 +20,7 @@ class Loginflow extends CI_Controller {
 			// 
 			// For more details see: http://developers.facebook.com/docs/opengraph
 			
-			$opengraph = 	array(
-								'type'				=> 'website',
-								'title'				=> 'My Awesome Site',
-								'url'				=> site_url(),
-								'image'				=> '',
-								'description'		=> 'The best site in the whole world'
-							);
 
-			$this->load->vars('opengraph', $opengraph);
 			$this->load->view('facebook_view');
 		}
 		
